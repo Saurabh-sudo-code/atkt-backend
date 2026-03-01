a414fae0fd75e1a5532f0d6a539e68f317987d5a
Saurabh Gupta <sgbuddy2003@gmail.com>
2026-03-01 19:01:09 +0530

    cnage in mailer.js

--- config/mailer.js
+++ config/mailer.js
@@ -2,11 +2,14 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // TRUE for 465
+  port: 587,
+  secure: false, // false for 587
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
